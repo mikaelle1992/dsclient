9a3636684621497baaaca18a47a094c360c224b6
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-06-12 14:44:31 -0300

    database seeding

--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repositories.ClientRepository;
@@ -15,8 +16,11 @@ public class ClientService {
 	@Autowired
 	public ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Client> findAll(){
 		return repository.findAll();
 	}
 
+	
+	
 }
