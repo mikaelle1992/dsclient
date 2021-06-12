21ee8fedce894c0eac5093286278c9997e41999b
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-06-12 11:42:10 -0300

    Category resource

---
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsclient.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsclient.entities.Client;
+
+@RestController 
+@RequestMapping(value = "/clients")
+public class ClientResource {
+	
+	@GetMapping
+	public ResponseEntity <List<Client>> findAll(){
+		List<Client> list = new ArrayList<>();
+		list.add(new Client(1L,"Ana","12345678",1200.00,2));
+		return ResponseEntity.ok().body(list);
+	}
+}
